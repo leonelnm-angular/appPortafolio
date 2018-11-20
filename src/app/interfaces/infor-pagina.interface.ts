c3c6f9aeebb78df29ad5352552886c1299b47e7b
leonavam <leonavas1995@gmail.com>
2018-11-21 00:12:15 +0100

    Añadiendo servicio con interface de datos

---
+++ src/app/interfaces/infor-pagina.interface.ts
@@ -0,0 +1,14 @@
+
+export interface InfoPagina {
+    
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  facebook?: string;
+  instagram?: string;
+  github?: string;
+  equipo_trabajo?: any[];
+
+}
+
+
