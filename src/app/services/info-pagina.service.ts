8b3838fd15e60234cb196cf74c3712a311d1ddcc
leonavam <leonavas1995@gmail.com>
2018-11-21 14:37:05 +0100

    Añadido nuevo servicio productos que muestra los productos con ayuda de firebase

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,35 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient) { 
 
-    //Leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
-      .subscribe ( (resp: InfoPagina) => {
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp)
+    this.cargarInfo();
+    this.cargarEquipo();
 
-      })
+  }
+
+  private cargarInfo(){
+     //Leer el archivo JSON
+     this.http.get('assets/data/data-pagina.json')
+     .subscribe ( (resp: InfoPagina) => {
+       this.cargada = true;
+       this.info = resp;
+
+     });
+  }
+
+  private cargarEquipo(){
+    this.http.get('https://portafolio-51713.firebaseio.com/equipo.json')
+    .subscribe ( (resp:any[]) => {
+     this.equipo = resp;
+     //console.log(resp)
+
+    })
 
 
 
-   }
+  }
+
 }
